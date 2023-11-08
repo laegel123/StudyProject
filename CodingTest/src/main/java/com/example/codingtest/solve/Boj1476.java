c03ecf66bc5bf5e43427a3e609c37b27f448146a
laegel <laegel@selper.co.kr>
2023-11-08 14:11:34 +0900

    브루트포스 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj1476.java
@@ -0,0 +1,48 @@
+package com.example.codingtest.solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int targetE = Integer.parseInt(st.nextToken());
+        int targetS = Integer.parseInt(st.nextToken());
+        int targetM = Integer.parseInt(st.nextToken());
+
+        int e = 0;
+        int s = 0;
+        int m = 0;
+
+        int year = 0;
+        while (true) {
+            if (e == targetE && s == targetS && m == targetM) {
+                break;
+            }
+
+            e++;
+            m++;
+            s++;
+
+            if (e == 16) {
+                e = 1;
+            }
+
+            if (s == 29) {
+                s = 1;
+            }
+
+            if (m == 20) {
+                m = 1;
+            }
+
+            year++;
+        }
+
+        System.out.println(year);
+    }
+}

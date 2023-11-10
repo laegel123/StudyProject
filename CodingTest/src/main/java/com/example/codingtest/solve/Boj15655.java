86b8bb370afbb556942b94909641f52747db26a6
laegel <laegel@selper.co.kr>
2023-11-10 10:31:49 +0900

    브루트포스 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj15655.java
@@ -0,0 +1,59 @@
+package com.example.codingtest.solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj15655 {
+    static int NUMBER;
+    static int LENGTH;
+    static int[] ARR;
+    static int[] MEMORY;
+    static StringBuilder SB = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        NUMBER = Integer.parseInt(st.nextToken());
+        LENGTH = Integer.parseInt(st.nextToken());
+
+        ARR = new int[NUMBER];
+        MEMORY = new int[LENGTH];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < NUMBER; i++) {
+            ARR[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(ARR);
+
+        dfs(0);
+
+        System.out.println(SB);
+        br.close();
+    }
+
+    static void dfs(int depth) {
+        if (LENGTH == depth) {
+            for (int val : MEMORY) {
+                SB.append(val).append(" ");
+            }
+
+            SB.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < NUMBER; i++) {
+            if (depth != 0 && (ARR[i] < MEMORY[depth - 1] || ARR[i] == MEMORY[depth - 1])) {
+                continue;
+            }
+
+            MEMORY[depth] = ARR[i];
+            dfs(depth + 1);
+        }
+    }
+
+
+}

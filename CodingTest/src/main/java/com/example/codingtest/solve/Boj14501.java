df794b70bc565cf2ca49e840de06aa5891fd36e3
laegel <laegel@selper.co.kr>
2023-11-14 12:22:06 +0900

    브루트포스 문제풀이

--- CodingTest/src/main/java/com/example/codingtest/solve/Boj14501.java
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj14501.java
@@ -9,7 +9,7 @@ import java.util.StringTokenizer;
 
 public class Boj14501 {
     static int T;
-    static int[][] TIME;
+    static int[] TIME;
     static int[] PAY;
     static List<Integer> PAY_LIST = new ArrayList<>();
     static int MAX_SUM = Integer.MIN_VALUE;
@@ -19,48 +19,33 @@ public class Boj14501 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         T = Integer.parseInt(br.readLine());
-        TIME = new int[T + 1][1];
-        PAY = new int[T + 1];
+        TIME = new int[T];
+        PAY = new int[T];
 
-        for (int i = 1; i < T + 1; i++) {
+        for (int i = 0; i < T; i++) {
             st = new StringTokenizer(br.readLine());
-            TIME[i][0] = Integer.parseInt(st.nextToken());
+            TIME[i] = Integer.parseInt(st.nextToken());
             PAY[i] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(1, 0);
+        dfs(0, 0);
 
         System.out.println(MAX_SUM);
         br.close();
     }
 
-    static void dfs(int day, int remainingTime) {
-        if (day == T) {
-            int sum = 0;
-            for (int i = 0; i < PAY_LIST.size(); i++) {
-                sum += PAY_LIST.get(i);
-            }
-
-            MAX_SUM = Math.max(MAX_SUM, sum);
+    static void dfs(int day, int pay) {
+        if (day >= T) {
+            MAX_SUM = Math.max(MAX_SUM, pay);
             return;
         }
 
-
-        for (int i = day; i < T + 1; i++) {
-            if (remainingTime == 0 && T - day >= TIME[i][0]) {
-                PAY_LIST.add(PAY[day]);
-                remainingTime = TIME[i][0];
-            } else {
-                PAY_LIST.add(0);
-            }
-
-            if (remainingTime > 0) {
-                dfs(day + 1, remainingTime - 1);
-            } else {
-                dfs(day + 1, remainingTime);
-            }
-
-            PAY_LIST.remove(day-1);
+        if (day + TIME[day] <= T) {
+            dfs(day + TIME[day], pay + PAY[day]);
+        } else {
+            dfs(day + TIME[day], pay);
         }
+
+        dfs(day + 1, pay);
     }
 }

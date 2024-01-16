9e18069214c8acb43138389c7111af540fc7c745
laegel <laegel1@gmail.com>
2024-01-16 23:33:48 +0900

    누적합 문제풀이

--- CodingTest/src/main/java/com/example/codingtest/solve/Boj25682.java
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj25682.java
@@ -13,51 +13,47 @@ public class Boj25682 {
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n][m];
-        int[][] reverse = new int[n][m];
+        int[][] startBArr = new int[n][m];
+        int[][] startWArr = new int[n][m];
         for (int i = 0; i < n; i++) {
             String[] str = br.readLine().split("");
             for (int j = 0; j < m; j++) {
                 // B로시작
-                if (str[j].equals("B") && (i + j) % 2 != 0) {
-                    arr[i][j] = 1;
-                }
-                if (str[j].equals("W") && (i + j) % 2 == 0) {
-                    arr[i][j] = 1;
+                if (str[j].equals("B") && (i + j) % 2 != 0
+                    || str[j].equals("W") && (i + j) % 2 == 0) {
+                    startBArr[i][j] = 1;
                 }
 
                 // W로시작
-                if (!str[j].equals("W") && (i + j) % 2 != 0) {
-                    reverse[i][j] = 1;
-                }
-                if (!str[j].equals("B") && (i + j) % 2 == 0) {
-                    reverse[i][j] = 1;
+                if (str[j].equals("W") && (i + j) % 2 != 0
+                    || str[j].equals("B") && (i + j) % 2 == 0) {
+                    startWArr[i][j] = 1;
                 }
 
             }
         }
 
-        int[][] changeArr = new int[n + 1][m + 1];
-        int[][] reverseArr = new int[n + 1][m + 1];
+        int[][] startBSumArr = new int[n + 1][m + 1];
+        int[][] startWSumArr = new int[n + 1][m + 1];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                changeArr[i + 1][j + 1] = changeArr[i + 1][j] + arr[i][j];
-                reverseArr[i + 1][j + 1] = reverseArr[i + 1][j] + reverse[i][j];
+                startBSumArr[i + 1][j + 1] = startBSumArr[i + 1][j] + startBArr[i][j];
+                startWSumArr[i + 1][j + 1] = startWSumArr[i + 1][j] + startWArr[i][j];
             }
         }
 
         for (int i = 1; i < m + 1; i++) {
             for (int j = 1; j < n + 1; j++) {
-                changeArr[j][i] += changeArr[j - 1][i];
-                reverseArr[j][i] += reverseArr[j - 1][i];
+                startBSumArr[j][i] += startBSumArr[j - 1][i];
+                startWSumArr[j][i] += startWSumArr[j - 1][i];
             }
         }
 
         int minCount = Integer.MAX_VALUE;
         for (int i = 0; i < n - k + 1; i++) {
             for (int j = 0; j < m - k + 1; j++) {
-                minCount = Math.min(minCount, changeArr[i + k][j + k] - changeArr[i + k][j] - changeArr[i][j + k]);
-                minCount = Math.min(minCount, reverseArr[i + k][j + k] - reverseArr[i + k][j] - reverseArr[i][j + k]);
+                minCount = Math.min(minCount, startBSumArr[i + k][j + k] - startBSumArr[i + k][j] - startBSumArr[i][j + k] + startBSumArr[i][j]);
+                minCount = Math.min(minCount, startWSumArr[i + k][j + k] - startWSumArr[i + k][j] - startWSumArr[i][j + k] + startWSumArr[i][j]);
             }
         }
 

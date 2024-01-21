c30a97658b6683a0adad10d57e7ac771574bcc64
laegel <laegel1@gmail.com>
2024-01-21 20:45:14 +0900

    백준 구현 문제 풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj1009.java
@@ -0,0 +1,37 @@
+package com.example.codingtest.solve;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // a 를 b 번 곱한다. (x)
+            int x = 1;
+            for (int i = 0; i < b; i++) {
+                x = x * a % 10;
+            }
+
+            if (x == 0) {
+                x = 10;
+            }
+            // 값 저장
+            sb.append(x).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}

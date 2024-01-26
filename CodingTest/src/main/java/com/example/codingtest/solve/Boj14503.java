06f04accd6dea6c7d8ca2598e67e45cee1bc55c4
laegel <laegel@selper.co.kr>
2024-01-26 16:31:57 +0900

    백준 DFS 문제풀이

--- CodingTest/src/main/java/com/example/codingtest/solve/Boj14503.java
+++ CodingTest/src/main/java/com/example/codingtest/solve/Boj14503.java
@@ -6,75 +6,64 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj14503 {
+    static int N, M, R, C, D;
+    static int[][] BOARD;
+    static int COUNT = 0;
+    static int[] DX = {-1, 0, 1, 0};
+    static int[] DY = {0, 1, 0, -1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] distanceX = {0, 1, 0, -1};
-        int[] distanceY = {1, 0, -1, 0};
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
-        int startX = Integer.parseInt(st.nextToken());
-        int startY = Integer.parseInt(st.nextToken());
-        int d = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        D = Integer.parseInt(st.nextToken());
 
-        int[][] room = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        BOARD = new int[N][M];
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                room[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                BOARD[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int count = 0;
-        while (true) {
-            // 현재 칸이 0인 경우 count ++, 1로 변경
-            if (room[startX][startY] == 0) {
-                count++;
-                room[startX][startY] = 1;
-            }
+        dfs(R, C, D);
 
-            // 주변 4칸 탐색
-            boolean isEmptyRoom = false;
-            int newX = 0;
-            int newY = 0;
-            for (int i = 0; i < 4; i++) {
-                int[] distance = getDistance(d);
-                if (room[startX + distance[0]][startY + distance[1]] == 0) {
-                    isEmptyRoom = true;
-                    newX = startX + distance[0];
-                    newY = startY + distance[1];
-                }
-            }
-
-            // 탐색 후 빈 칸 없는 경우 후진 and 벽이면 멈춤
-
-            // 탐색 후 빈 칸 있는 경우 반시계 방향으로 90도 회전, 한 칸 전진
-        }
+        System.out.println(COUNT);
+        br.close();
     }
 
-    static int[] getDistance(int d) {
-        int[] result = new int[3];
-
-        if (d == 0) {
-            result[0] = 1;
-            result[1] = -1;
-            result[2] = 3;
-        } else if (d == 1) {
-            result[0] = -1;
-            result[1] = -1;
-            result[2] = 0;
-        } else if (d == 2) {
-            result[0] = -1;
-            result[1] = 1;
-            result[2] = 1;
-        } else if (d == 3) {
-            result[0] = 1;
-            result[1] = 1;
-            result[2] = 0;
+    static void dfs(int x, int y, int direct) {
+        if (BOARD[x][y] == 0) {
+            COUNT++;
+            BOARD[x][y] = 2;
         }
 
-        return result;
+        for (int i = 0; i < 4; i++) {
+            direct = (direct + 3) % 4;
+            int newX = x + DX[direct];
+            int newY = y + DY[direct];
+
+            if (newX >= 0 && newY >= 0 && newX < N && newY < M && BOARD[newX][newY] == 0) {
+                dfs(newX, newY, direct);
+
+                return;
+            }
+        }
+
+        int behind = (direct + 2) % 4;
+        int newX = x + DX[behind];
+        int newY = y + DY[behind];
+
+        if (newX >= 0 && newX < N && newY >= 0 && newY < M && BOARD[newX][newY] != 1) {
+            dfs(newX, newY, direct);
+        }
+
     }
+
+
 }

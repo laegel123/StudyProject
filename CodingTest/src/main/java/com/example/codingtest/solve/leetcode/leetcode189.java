4f0b0d23ae1a0500e58b3d7e57c8faf58751a1b3
laegel <laegel@selper.co.kr>
2024-03-27 18:34:42 +0900

    리트코드 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/leetcode/leetcode189.java
@@ -0,0 +1,28 @@
+package com.example.codingtest.solve.leetcode;
+
+/**
+ * @author laegel
+ * @version 1.0
+ * @since 2024-03-27
+ */
+public class leetcode189 {
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+    }
+
+    public static void rotate(int[] nums, int k) {
+        k %= nums.length;
+        int n = nums.length;
+        swap(nums, 0, nums.length - 1);
+        swap(nums, 0, k - 1);
+        swap(nums, k, nums.length - 1);
+    }
+
+    static void swap(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
+        }
+    }
+}

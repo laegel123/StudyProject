bec1d4057023778f4427ffbb1eb382fcdff5eb87
laegel <laegel@selper.co.kr>
2024-07-02 16:53:36 +0900

    리트코드 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/leetcode/leetcode18.java
@@ -0,0 +1,62 @@
+package com.example.codingtest.solve.leetcode;
+
+import java.util.*;
+
+/**
+ * @author laegel
+ * @version 1.0
+ * @since 2024-07-02
+ */
+class Solution18 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> resultList = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        
+        for (int i = 0; i < nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                
+                int left = j + 1;
+                int right = nums.length - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + (long) nums[j] + (long) nums[left] + (long) nums[right];
+
+                    if (sum < target) {
+                        left++;
+                    }
+                    else if (sum > target) {
+                        right--;
+                    }
+                    else {
+                        resultList.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
+                        left++;
+                        right--;
+                    }
+                }
+                
+            }
+        }
+        
+        
+        
+        return resultList;
+    }
+}
+
+public class leetcode18 {
+    public static void main(String[] args) {
+        Solution18 solution = new Solution18();
+        int[] nums = {1000000000,1000000000,1000000000,1000000000};
+        int target = -294967296;
+        List<List<Integer>> resultList = solution.fourSum(nums, target);
+        System.out.println(resultList);
+    }
+}

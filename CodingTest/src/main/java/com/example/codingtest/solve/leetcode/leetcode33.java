9b9edf732617da324b7e82f701b7354a98f346a8
laegel <laegel1@gmail.com>
2024-07-16 23:49:56 +0900

    리트코드 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/leetcode/leetcode33.java
@@ -0,0 +1,41 @@
+package com.example.codingtest.solve.leetcode;
+
+/**
+ * @author laegel
+ * @version 1.0
+ * @since 2024-07-16
+ */
+class Solution33 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) return mid;
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
+
+public class leetcode33 {
+    public static void main(String[] args) {
+
+    }
+}

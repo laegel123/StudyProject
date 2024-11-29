ecb487c108fe5751464220118fd1485a929ed11f
laegel <laegel@selper.co.kr>
2024-11-29 13:46:27 +0900

    Title : 코드업문제풀이 코드업문제풀이

--- CodingTest/src/main/java/com/example/codingtest/solve/codeup/codeup1901.java
+++ CodingTest/src/main/java/com/example/codingtest/solve/codeup/codeup1901.java
@@ -1,8 +1,29 @@
 package com.example.codingtest.solve.codeup;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class codeup1901 {
-    public static void main(String[] args) {
-        
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        recur(1);
+
+        br.close();
+
+    }
+
+
+    static void recur(int n) {
+        if (n > N) return;
+
+        System.out.println(n);
+        recur(n + 1);
+
     }
 
 }

69132298210035f75a4522b3fd4b0c6d8ace4403
laegel <laegel@selper.co.kr>
2024-12-23 15:09:14 +0900

    Title : boj 문제풀이 문제풀이

---
+++ CodingTest/src/main/java/com/example/codingtest/solve/boj/Boj10816.java
@@ -0,0 +1,65 @@
+package com.example.codingtest.solve.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj10816 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            sb.append(upperBound(arr, target) - lowerBound(arr, target)).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    static int upperBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    static int lowerBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+}
